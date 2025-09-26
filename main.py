74b8e5910b3d813703abea40fbcd8075e4867ac1
Medveen N. Alexis <nauraalexis777@gmail.com>
2025-09-26 23:07:05 +0000

    modified code

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ print ("Welcome to Meddy's store!")
 name= input("What is your name?")
 print("Hello, " + name + "! Thank you for coming here!")
 age= input ("How old are you?")
+
+
+print ("Here is our menu")
+print ("1.Chips")
+print ("2. Drinks")
+print ("3.Candy")
+
 need= input("Great, what would you like to buy today?.")
+
+
+print("Great choice, "+ name +" We'll get you some" + need + ".")
+
+print("Thank you for shopping at Meddy's store ! Come see us again") 
